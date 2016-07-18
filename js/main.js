b0affb468d955fd9c79b48ab434f31a16babca00
Sasha <skif@ki-technology.ru>
2016-07-19 01:42:20 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function typesOfArray(arr) {
+    var str = 0;
+    var obj = 0;
+    var num = 0;
+    var bol = 0;
+    var nul = 0;
+    var und = 0;
+
+    console.info(arr);
+
+    for(var i = 0; i < arr.length; i++) {
+
+        if(arr[i] !== null) {
+            switch(typeof(arr[i])) {
+                case 'string': str++; break;
+                case 'number': num++; break;
+                case 'object': obj++; break;
+                case 'boolean': bol++; break;
+                case 'undefined': und++; break;
+            }
+        }
+        else {
+          nul++;  
+        } 
+    }
+        console.log(
+            'Array types stat: ',
+            '\nstring: ' + str, 
+            '\nnumber: ' + num, 
+            '\nobject: ' + obj, 
+            '\nboolean: ' + bol, 
+            '\nnull: ' + nul, 
+            '\nundefined: ' + und
+        );
+
+}
+typesOfArray([1, 'a', 'b', {}, {}, null, null, undefined, true, false]);
