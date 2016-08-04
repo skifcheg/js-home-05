3a99f6eb8a3b2fa2db715b6051d5b4ae87522c56
admin <admin@MacBook-Pro-admin.local>
2016-08-04 21:25:05 +0300

    add

--- js/main.js
+++ js/main.js
@@ -42,21 +42,80 @@ console.groupEnd("--------------------");
 console.group("---------- 2 ----------");
 
 
+var rangeToArray = function(rangeStart, rangeStop, rangeStep){
+    var arr = []; var index = 0;
 
+    if(rangeStop >= rangeStart){
+        for(var i = rangeStart; i <= rangeStop; i = i + rangeStep){
+            arr[index] = i;
+            index++;
+        }
+    }else{
+        for(var i = rangeStart; i >= rangeStop; i = i - rangeStep){
+            arr[index] = i;
+            index++;
+        }
+    }
+    return arr;
+};
 
+var sumArray = function(arr){
+    var sum = 0;
+    arr.forEach(function(item){
+        sum = sum + item;
+    });
+    return sum;
+};
+
+var multiplyArray = function(arr){
+    var sum = 1;
+    arr.forEach(function(item){
+        sum = sum * item;
+    });
+    return sum;
+};
+
+var reverseArray = function(arr){
+    return arr.reverse();
+};
 
 console.groupEnd("--------------------");
 console.group("---------- 3 ----------");
 
+(function(){
 
+    var text = prompt('write text please');
+    var rightText, percent;
+    var punctuations = [',','.','!','?','-',':',';'];
+    var arrP = []; var j = 0;
 
+    if( text.length < 1 || text.length > 140 )  text = prompt('write text from 1 to 140 characters');
+
+    var arr = text.split("");
+
+    for(i = 0; i < arr.length; i++){
+
+        if (punctuations.indexOf(arr[i]) != -1 ){
+            arrP[j] = i;
+            j++;
+        }
+
+    }
+
+    rightText = arr.join("");
+    console.log(arr, arrP);
+
+    console.log("Hey, here's Your original message: %s \n We've found some mistakes, so corrected them for You: %s \n %f, of the text are punctuation marks!", text, rightText, percent);
+
+})();
 
 
 console.groupEnd("--------------------");
 console.group("---------- 4 ----------");
 
 function deepEqual(a, b){
-    var str1 = str2 = "";
+    var str1 = "";
+    var str2 = "";
     for ( var key in a){
         str1 += a[key] + ":" + key;
     }
